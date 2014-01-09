eace890068834005abb1f98724027e7c0e8c8a78
Nathan Ziarek <nziarek@gmail.com>
2014-01-08 18:14:58 -0600

    Release v0.1.1

--- source/scripts/init.js
+++ source/scripts/init.js
@@ -18,6 +18,6 @@ function fadeIn() {
 	var elem = bodyLI.pop();
 	$(elem).addClass("state-loaded");
 	if(bodyLI.length > 0) {
-		window.setTimeout(fadeIn, 500);
+		window.setTimeout(fadeIn, 50);
 	}
 }
