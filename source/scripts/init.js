1b5c298afaac36aa2401b59a0a7afccf048b1cbe
Nathan Ziarek <nziarek@gmail.com>
2014-01-08 18:11:40 -0600

    Changing fade in style to one by one

--- source/scripts/init.js
+++ source/scripts/init.js
@@ -1,3 +1,5 @@
+var bodyLI = [];
+
 WebFontConfig = {
 	google: {
 		families: ['Alegreya+Sans:100,400,700,400italic,700italic']
@@ -6,6 +8,16 @@ WebFontConfig = {
 		$("body.home ul").packery({
 			itemSelector: 'li',
 			gutter: 30
-		}).addClass("state-ready");
+		});
+		bodyLI = $('body.home ul li').get().reverse();
+		fadeIn();
 	}
-};
+}
+
+function fadeIn() {
+	var elem = bodyLI.pop();
+	$(elem).addClass("state-loaded");
+	if(bodyLI.length > 0) {
+		window.setTimeout(fadeIn, 500);
+	}
+}
